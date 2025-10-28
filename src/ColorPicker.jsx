dbdd143b1e71a6a71246c4030343d101719e4424
Wakoya Daba <waaqoyyaadhaabaa@gmail.com>
2025-10-29 00:18:51 +0300

    Refactor handleColorChange in ColorPicker component
    Refactor handleColorChange function for better readability.

--- src/ColorPicker.jsx
+++ src/ColorPicker.jsx
@@ -3,11 +3,9 @@ import React, { useState } from 'react'
 function ColorPicker() {
   
   const [color,setColor]=useState("#ffffff");
-  
 function handleColorChange(e)
 {
   setColor(e.target.value);
-  
 }
   return (
     <div className='color-picker-container'>
